538eb846e521f6760bb45c1298f7af01966ab350
imchengr <imchengr@gmail.com>
2016-03-13 23:26:04 +0800

    加入剩餘五個test case 1)第一集_1本_第二集_1本_第三集_1本() 2)第一集_1本_第二集_1本_第三集_1本_第四集_1本() 3)第一集_1本_第二集_1本_第三集_1本_第四集_1本_第五集_1本() 4)第一集_1本_第二集_1本_第三集_2本() 5)第一集_1本_第二集_2本_第三集_2本()

--- PotterShoppingCart.Tests/CashierUnitTest.cs
+++ PotterShoppingCart.Tests/CashierUnitTest.cs
@@ -44,5 +44,108 @@ namespace PotterShoppingCart.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CartOrder_第一集_1本_第二集_1本_第三集_1本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1},
+                new CartModel{BookName="HP2", Quantity=1},
+                new CartModel{BookName="HP3", Quantity=1}
+            };
+
+            var expected = 270;
+            Cashier cashier = new Cashier(lsCartModel);
+
+            //Act
+            var actual = cashier.Check();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CartOrder_第一集_1本_第二集_1本_第三集_1本_第四集_1本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1},
+                new CartModel{BookName="HP2", Quantity=1},
+                new CartModel{BookName="HP3", Quantity=1},
+                new CartModel{BookName="HP4", Quantity=1}
+            };
+
+            var expected = 320;
+            Cashier cashier = new Cashier(lsCartModel);
+
+            //Act
+            var actual = cashier.Check();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CartOrder_第一集_1本_第二集_1本_第三集_1本_第四集_1本_第五集_1本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1},
+                new CartModel{BookName="HP2", Quantity=1},
+                new CartModel{BookName="HP3", Quantity=1},
+                new CartModel{BookName="HP4", Quantity=1},
+                new CartModel{BookName="HP5", Quantity=1},
+            };
+
+            var expected = 375;
+            Cashier cashier = new Cashier(lsCartModel);
+
+            //Act
+            var actual = cashier.Check();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CartOrder_第一集_1本_第二集_1本_第三集_2本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1},
+                new CartModel{BookName="HP2", Quantity=1},
+                new CartModel{BookName="HP3", Quantity=2}
+            };
+
+            var expected = 370;
+            Cashier cashier = new Cashier(lsCartModel);
+
+            //Act
+            var actual = cashier.Check();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CartOrder_第一集_1本_第二集_2本_第三集_2本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1},
+                new CartModel{BookName="HP2", Quantity=2},
+                new CartModel{BookName="HP3", Quantity=2}
+            };
+
+            var expected = 460;
+            Cashier cashier = new Cashier(lsCartModel);
+
+            //Act
+            var actual = cashier.Check();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

d833c624d97c96246646550468331febe10abf43
imchengr <imchengr@gmail.com>
2016-03-13 20:05:40 +0800

    create solution

---
+++ PotterShoppingCart.Tests/CashierUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PotterShoppingCart;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class CashierUnitTest
+    {
+        [TestMethod]
+        public void CartOrder_第一集_1本()
+        {
+            //Arrange
+            List<CartModel> lsCartModel = new List<CartModel>(){
+                new CartModel{BookName="HP1", Quantity=1}
+            };
+
+            var expected = 100;
+            Cashier cashier = new Cashier();
+
+            //Act
+            var actual = cashier.check(lsCartModel);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ PotterShoppingCart/CartModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class CartModel
+    {
+        public string BookName { get; set; }
+        public int Quantity { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        public int Discount { get; set; }
+    }
+}
